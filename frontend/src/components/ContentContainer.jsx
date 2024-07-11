6c9569001e47ee0b928fb07dd24c77430731436d
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-07-11 01:58:48 -0400

    Fixing contentContainer.jsx for different screen sizes

--- frontend/src/components/ContentContainer.jsx
+++ frontend/src/components/ContentContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContentContainer = (props) => {
 	return (
-		<div className={`mx-4 lg:mx-10 mt-12 ${props.className}`}>
+		<div className={`md:mx-16 xl:mx-60 my-12 ${props.className}`}>
 			{props.children}
 		</div>
 	);
