e86a30c4a2a720aec18b71ef3c2e9af1af590282
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-07-11 01:57:05 -0400

    Adding a footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <div className='flex justify-center items-center text-back_light_blue bg-dark_blue h-[80px]'>
+        <p>Emily Ho ©2024</p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
