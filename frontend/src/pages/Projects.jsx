b40a3a35f8a310689428b659cc033eb95e5051d2
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-07-12 19:45:58 -0400

    Creating a projects page

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ContentContainer from '../components/ContentContainer'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <>
+      <ContentContainer>
+        <div className='bg-light_blue text-dark_blue'>
+          <h2 className='bg-dark_blue text-back_light_blue text-center'>Healthcare Web Application</h2>
+          <div className='p-5'>
+            <p>Tech Stack: MongoDB, React.JS, Node.JS, Express.JS, Flask, Python</p>
+            <p className='pt-4'>Healthcare web application for the PaRx Foundation Company.</p>
+          </div>
+        </div>
+      </ContentContainer>
+    
+    </>
   )
 }
 
