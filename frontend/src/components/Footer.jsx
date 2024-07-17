6a8a393609b2e31a28d7a491c55e8493dca8206d
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-07-17 02:43:47 -0400

    Adding social icons to footer with links

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
     <>
-      <div className='flex justify-center items-center text-back_light_blue bg-dark_blue h-[80px]'>
-        <p>Emily Ho ©2024</p>
+      <div className='flex justify-between items-center text-back_light_blue bg-dark_blue h-[80px]'>
+        <p className='flex-1 text-center text-lg'>Emily Ho ©2024</p>
+        <div className="space-x-4 pr-8 text-2xl">
+          <a href="https://github.com/Emilyho11" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="mailto:emilyy.ho@mail.utoronto.ca">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+          <a href="https://www.linkedin.com/in/emily-ho-cs/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </div>
+
       </div>
     </>
   )
