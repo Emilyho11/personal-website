ee6b2f075e39376cab17559b32dd35b27155a94c
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-07-22 10:52:56 -0400

    Prevent users from accessing my code through inspect elements. Uncomment it later.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,35 @@ import Projects from './pages/Projects.jsx'
 import HobbiesGame from './pages/HobbiesGame.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer.jsx'
+import { useEffect } from 'react'
 
 function App() {
+
+  // useEffect(() => {
+  //   // Function to disable right-click
+  //   const handleContextMenu = (event) => {
+  //     event.preventDefault();
+  //   };
+    
+  //   // Function to disable specific keyboard shortcuts
+  //   const handleKeyDown = (event) => {
+  //     if (event.keyCode === 123 || // F12
+  //         (event.ctrlKey && event.shiftKey && (event.keyCode === 73 || event.keyCode === 74)) || // Ctrl+Shift+I or Ctrl+Shift+J
+  //         (event.ctrlKey && event.keyCode === 85)) { // Ctrl+U
+  //       event.preventDefault();
+  //     }
+  //   };
+    
+  //   // Add event listeners
+  //   document.addEventListener('contextmenu', handleContextMenu);
+  //   document.addEventListener('keydown', handleKeyDown);
+
+  //   // Clean up event listeners on unmount
+  //   return () => {
+  //     document.removeEventListener('contextmenu', handleContextMenu);
+  //     document.removeEventListener('keydown', handleKeyDown);
+  //   };
+  // }, []);
 
   return (
     <>
