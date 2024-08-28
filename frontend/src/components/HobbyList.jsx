17f5427a65b4a78eb42323ce84ff1c0fb7ce622e
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-08-27 23:38:31 -0400

    Adding MoveU hobby to my hobbies list

--- frontend/src/components/HobbyList.jsx
+++ frontend/src/components/HobbyList.jsx
@@ -4,6 +4,7 @@ import DragonBoat from '../assets/images/hobbies/dragonBoat.jpg';
 import WushuCompetition from '../assets/images/hobbies/wushu_competition.jpg';
 import Flute from '../assets/images/hobbies/kiss_the_rain_flute.jpg';
 import CheerPerformance from '../assets/images/hobbies/performance.jpg';
+import moveU from '../assets/images/hobbies/tabling_moveU.jpg';
 import Swimming from '../assets/images/hobbies/swimming.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
@@ -29,42 +30,38 @@ export const HobbyList = () => {
     {
       hobbyName: 'Dragon Boat Team',
       description: 'I was part of the Dragon Boat team at Crystal Claire Cosmetics Inc. during my 1st co-op term as a Software Engineer. We won GOLD at the Toronto Dragon Boat Festival! It was a great experience to work with my colleagues outside of the office and to build a strong team spirit.',
-      extraInfo: [
-      ],
       date: '2023',
       image: DragonBoat
     },
     {
       hobbyName: 'Wushu (Kung Fu)',
       description: 'I was part of the Wushu team, participating in competitions, performances, Toronto parades, and many more. We practice acrobatics, and use weapons such as swords, staffs, and spears. I have been practicing Wushu for over 10 years, and it has taught me discipline, respect, and perseverance.',
-      extraInfo: [
-      ],
       date: '2014-present',
       image: WushuCompetition
     },
     {
       hobbyName: 'Cheerleading Team',
       description: 'Particpated in the uoft cheer team, and performed at various events, such as basketball games, and performances. It was a great experience to work with my teammates, and to build a strong team spirit.',
-      extraInfo: [
-      ],
       date: '2023-2024',
       image: CheerPerformance
     },
     {
       hobbyName: 'Flute',
       description: 'I was part of concert band from grade 6 to grade 10, playing the flute. I enjoyed playing in the band, and performing at school concerts, music competitions, and music festivals. I have played many pieces, such as "The Lion King", "Pirates of the Caribbean", and "The Incredibles".',
-      extraInfo: [
-      ],
       date: '2014-2019',
       image: Flute
     },
     {
       hobbyName: 'Swimming',
       description: 'I have been swimming and taking swimming lessons since I was 6, and I aimed to get my lifeguard certifications. I received my Bronze Medallion, but after that, COVID hit and prevented me from completing it. However, to this day, swimming is still a hobby I enjoy.',
-      extraInfo: [
-      ],
       date: '2009-2019',
       image: Swimming
+    },
+    {
+      hobbyName: 'MoveU',
+      description: 'MoveU is a UofT Athletics club that promotes healthy living and physical activity. As a first year representative for this club, I planned and participated in various events and activities such as dodgeball, weekly workouts, SheMoves, and many more! It was a great way to stay active and meet new people, while ensuring that students are taking care of their physical health.',
+      date: '2021-2022',
+      image: moveU
     }
   ]
 
@@ -80,10 +77,19 @@ export const HobbyList = () => {
         <div key={index} className='bg-dark_blue p-6 text-back_light_blue rounded shadow'>
           <h3 className="text-2xl">{hobby.hobbyName}</h3>
           <p className="text-m font-semibold">{hobby.date}</p>
-          <div className='flex gap-6 items-start'>
+          <div className='md:flex gap-6 items-start'>
+          <p className="mt-2">{hobby.description}</p>
+            {hobby.image && (
+              <img
+                src={hobby.image}
+                alt={`${hobby.hobbyName} image`}
+                className="w-1/2 h-auto lg:max-h-[450px] object-cover border border-slate-400"
+                style={{ aspectRatio: '16/9' }}
+              />
+            )}
+          </div>
           {hobby.extraInfo && (
             <div className='flex-1 pt-4'>
-            <p className="mt-2">{hobby.description}</p>
               <button
                 onClick={() => toggleExpand(index)}
                 className="mt-2 py-2 text-light_blue hover:underline hover:text-links rounded"
@@ -99,15 +105,6 @@ export const HobbyList = () => {
                 </ul>
             )}
           </div>)}
-            {hobby.image && (
-              <img
-                src={hobby.image}
-                alt={`${hobby.hobbyName} image`}
-                className="w-1/2 h-auto lg:max-h-[450px] object-cover border border-slate-400"
-                style={{ aspectRatio: '16/9' }}
-              />
-            )}
-          </div>
         </div>
       ))}
     </div>
