2143f9e30149e5fe85068e0efbe5613c53cc8cfc
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-08-29 01:22:21 -0400

    Implemented a music note game with hobbies

--- frontend/src/components/HobbyGame.jsx
+++ frontend/src/components/HobbyGame.jsx
@@ -1,34 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import ContentContainer from '../components/ContentContainer';
 import MusicNote1 from '../assets/images/1musicnote.png';
+import MusicNote2 from '../assets/images/2musicnote.png';
+import MusicNote3 from '../assets/images/3musicnote.png'; // Add new music note images
+import MusicNote4 from '../assets/images/4musicnote.png';
+import MusicNote5 from '../assets/images/5musicnote.png';
 import HobbyPopup from '../components/HobbyPopup';
-import Piano from '../assets/images/hobbies/piano.jpg';
+import hobbies from './AccessHobbies.jsx';
+import EmptyPopup from '../components/EmptyPopup.jsx';
 
 const HobbyGame = () => {
-  const [position, setPosition] = useState({ top: 0, left: 0 });
-  const [visible, setVisible] = useState(true);
+  const [position, setPosition] = useState([
+    { top: 0, left: 0 },
+    { top: 0, left: 0 },
+    { top: 0, left: 0 },
+    { top: 0, left: 0 },
+    { top: 0, left: 0 }
+  ]);
+  const [visible, setVisible] = useState([true, true, true, true, true]);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [currentHobbyIdx, setCurrentHobbyIdx] = useState(0);
+  const [score, setScore] = useState(0);
+  const [randomHobbyIdx, setRandomHobbyIdx] = useState([0, 0, 0, 0, 0]);
+  const [clickedHobbies, setClickedHobbies] = useState(new Set());
+  const [emptyPopupIdx, setEmptyPopupIdx] = useState(null);
 
   useEffect(() => {
-    const randomizePosition = () => {
-      const top = Math.random() * (window.innerHeight - 100); // Subtracting image height
-      const left = Math.random() * (window.innerWidth - 100); // Subtracting image width
-      setPosition({ top, left });
+    const randomizePositions = () => {
+      const newPositions = position.map(() => ({
+        top: Math.random() * (window.innerHeight - 200), // Subtracting image height
+        left: Math.random() * (window.innerWidth - 100) // Subtracting image width
+      }));
+      setPosition(newPositions);
     };
 
-    randomizePosition();
+    const randomizeVisibility = () => {
+      setVisible(visible.map(() => Math.random() > 0.5));
+    };
+
+    randomizePositions();
+    randomizeVisibility();
 
     const interval = setInterval(() => {
-      randomizePosition();
-      setVisible(false);
-      setTimeout(() => setVisible(true), 1000); // Time for disappear animation
-    }, 2000); // Change position every 2 seconds
+      randomizePositions();
+      randomizeVisibility();
+    }, 2000); // Change position and visibility every 2 seconds
 
     return () => clearInterval(interval);
   }, []);
 
-  const handleNoteClick = () => {
+  useEffect(() => {
+    // Randomize the hobby index for each music note when the component mounts
+    const randomIndices = Array.from({ length: 5 }, () => Math.floor(Math.random() * hobbies.length));
+    setRandomHobbyIdx(randomIndices);
+    // Randomly select one music note to display the empty popup
+    setEmptyPopupIdx(Math.floor(Math.random() * 5));
+  }, []);
+
+  const handleNoteClick = (noteIndex) => {
+    if (noteIndex === emptyPopupIdx) {
+      setIsPopupOpen(true);
+      setCurrentHobbyIdx(null);
+      setScore(0);
+      setClickedHobbies(new Set());
+      return;
+    }
+
+    const hobbyIdx = randomHobbyIdx[noteIndex];
+    if(!clickedHobbies.has(hobbyIdx)) {
+      setScore(score + 1);
+      setClickedHobbies(new Set(clickedHobbies.add(hobbyIdx)));
+    }
+    setCurrentHobbyIdx(hobbyIdx);
     setIsPopupOpen(true);
+    if (score + 1 === hobbies.length) {
+      alert('You have found all my hobbies!');
+    }
   };
 
   const closePopup = () => {
@@ -37,52 +84,81 @@ const HobbyGame = () => {
 
   return (
     <ContentContainer>
-        <h2>Try to catch as many music notes as you can to see all my hobbies and extracurricular activities!</h2>
-        
-        {/* {hobbies.map((hobby, index) => (
-            <div key={index} className='bg-gray-200 p-4 rounded shadow'>
-              <HobbyPopup hobby={hobby} />
-            </div>
-        ))} */}
-        
-        {visible && (
-          <button
-            onClick={handleNoteClick}
-            className="focus:outline-none absolute"
-            style={{ top: `${position.top}px`, left: `${position.left}px`, transition: 'top 1s, left 1s' }}
-          >
-            <img src={MusicNote1} alt="button image" className="w-1/12 h-full object-cover animate-fade-out"></img>
-          </button>
-        )}
-        
-        {isPopupOpen && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <div className="bg-white p-8 rounded">
+      <div className='text-center'>
+        <h2>Score: {score} / 15</h2>
+        <h3>Try to catch as many music notes as you can to see all my hobbies and extracurricular activities!</h3>
+      </div>
+
+      {visible[0] && (
+        <button
+          onClick={() => handleNoteClick(0)}
+          className="focus:outline-none absolute"
+          style={{ top: `${position[0].top}px`, left: `${position[0].left}px`, transition: 'top 1s, left 1s' }}
+        >
+          <img src={MusicNote1} alt="button image" className="w-10 h-full object-cover animate-fade-out"></img>
+        </button>
+      )}
+
+      {visible[1] && (
+        <button
+          onClick={() => handleNoteClick(1)}
+          className="focus:outline-none absolute"
+          style={{ top: `${position[1].top}px`, left: `${position[1].left}px`, transition: 'top 1s, left 1s' }}
+        >
+          <img src={MusicNote2} alt="button image" className="w-1/12 h-full object-cover animate-fade-out"></img>
+        </button>
+      )}
+
+      {visible[2] && (
+        <button
+          onClick={() => handleNoteClick(2)}
+          className="focus:outline-none absolute"
+          style={{ top: `${position[2].top}px`, left: `${position[2].left}px`, transition: 'top 1s, left 1s' }}
+        >
+          <img src={MusicNote3} alt="button image" className="w-1/12 h-full object-cover animate-fade-out"></img>
+        </button>
+      )}
+
+      {visible[3] && (
+        <button
+          onClick={() => handleNoteClick(3)}
+          className="focus:outline-none absolute"
+          style={{ top: `${position[3].top}px`, left: `${position[3].left}px`, transition: 'top 1s, left 1s' }}
+        >
+          <img src={MusicNote4} alt="button image" className="w-1/12 h-full object-cover animate-fade-out"></img>
+        </button>
+      )}
+
+      {visible[4] && (
+        <button
+          onClick={() => handleNoteClick(4)}
+          className="focus:outline-none absolute"
+          style={{ top: `${position[4].top}px`, left: `${position[4].left}px`, transition: 'top 1s, left 1s' }}
+        >
+          <img src={MusicNote5} alt="button image" className="w-1/12 h-full object-cover animate-fade-out"></img>
+        </button>
+      )}
+
+      {isPopupOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white p-8 rounded">
+            {currentHobbyIdx === null ? (
+              <EmptyPopup />
+            ) : (
               <HobbyPopup
-              hobby='Piano'
-              description='Piano is one of my favorite hobbies since I was little.
-              I have been taking lessons at the age of 6, and I am continuing my playing to this day.
-              I enjoy performing in front of others at school concerts, recitals, and retirement homes,
-              as seeing others smile makes me happy. Playing piano not only brings me joy, but it
-              helps me relax during stressful times.
-
-              Some Songs I play:
-              Endless Love - Lionel Richie and Diana Ross
-              Night Changes - One Direction
-              Right Here Waiting
-              What a Wonderful World
-              Moon Represents my Heart - Teresa Teng
-              I Want It That Way - Backstreet Boys
-              River Flows In You - Yiruma
-              Colors of the Wind - Pocahontas (Disney)'
-              date='2006-present'
-              image={Piano}/>
-              <button onClick={closePopup} className="mt-4 px-4 py-2 bg-dark_blue hover:bg-slate-600 text-white rounded">Close</button>
-            </div>
+                hobby={hobbies[currentHobbyIdx].hobbyName}
+                description={hobbies[currentHobbyIdx].description}
+                date={hobbies[currentHobbyIdx].date}
+                image={hobbies[currentHobbyIdx].image}
+                extraInfo={hobbies[currentHobbyIdx].extraInfo}
+              />
+            )}
+            <button onClick={closePopup} className="mt-4 px-4 py-2 bg-dark_blue hover:bg-slate-600 text-white rounded">Close</button>
           </div>
-        )}
-      </ContentContainer>
-  )
-}
+        </div>
+      )}
+    </ContentContainer>
+  );
+};
 
-export default HobbyGame
+export default HobbyGame;
