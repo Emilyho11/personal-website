90b8608f21110a4fbeba7099fc77819839b0cb16
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-09-23 21:53:33 -0400

    Adding new hobbies and images

--- frontend/src/components/AccessHobbies.jsx
+++ frontend/src/components/AccessHobbies.jsx
@@ -3,8 +3,9 @@ import DragonBoat from '../assets/images/hobbies/dragonBoat.jpg';
 import WushuCompetition from '../assets/images/hobbies/wushu_competition.jpg';
 import Flute from '../assets/images/hobbies/kiss_the_rain_flute.jpg';
 import CheerPerformance from '../assets/images/hobbies/performance2.jpg';
-import moveU from '../assets/images/hobbies/tabling_moveU.jpg';
+import moveU from '../assets/images/hobbies/end_of_year_celebration_moveU.jpg';
 import Swimming from '../assets/images/hobbies/swimming.jpg';
+import Skating from '../assets/images/hobbies/skating.jpg';
 import Horseback from '../assets/images/hobbies/horse_back_riding.jpg';
 import SkateBoarding from '../assets/images/hobbies/skateboarding.jpg';
 import Saxophone from '../assets/images/hobbies/saxophone.jpg';
@@ -24,67 +25,67 @@ const hobbies = [
         'Colors of the Wind - Pocahontas (Disney)',
         ],
         date: '2009-present',
-        image: Piano
+        image: [Piano]
     },
     {
         hobbyName: 'Dragon Boat Team',
         description: 'I was part of the Dragon Boat team at Crystal Claire Cosmetics Inc. during my 1st co-op term as a Software Engineer. We won GOLD at the Toronto Dragon Boat Festival! It was a great experience to work with my colleagues outside of the office and to build a strong team spirit.',
         date: '2023',
-        image: DragonBoat
+        image: [DragonBoat]
     },
     {
         hobbyName: 'Wushu (Kung Fu)',
         description: 'I was part of the Wushu team, participating in competitions, performances, Toronto parades, and many more. We practice acrobatics, and use weapons such as swords, staffs, and spears. I have been practicing Wushu for over 10 years, and it has taught me discipline, respect, and perseverance.',
         date: '2014-present',
-        image: WushuCompetition
+        image: [WushuCompetition]
     },
     {
         hobbyName: 'Cheerleading Team',
         description: 'Particpated in the uoft cheer team, and performed at various events, such as basketball games, and performances. It was a great experience to work with my teammates, and to build a strong team spirit.',
         date: '2022-2023, 2023-2024',
-        image: CheerPerformance
+        image: [CheerPerformance]
     },
     {
         hobbyName: 'Flute',
         description: 'I was part of concert band from grade 6 to grade 10, playing the flute. I enjoyed playing in the band, and performing at school concerts, music competitions, and music festivals. I have played many pieces, such as "The Lion King", "Pirates of the Caribbean", and "The Incredibles".',
         date: '2014-2019',
-        image: Flute
+        image: [Flute]
     },
     {
         hobbyName: 'Saxophone',
         description: 'I used to play the saxophone after I learned to play the flute because I heard that both instruments have very similar fingerings. I wanted to give it a try!',
         date: '2018-2019',
-        image: Saxophone
-    },
-    {
-        hobbyName: 'Guitar',
-        description: 'I took guitar lessons.',
-        date: '2018-2019',
-        image: Flute
+        image: [Saxophone]
     },
     {
         hobbyName: 'Swimming',
-        description: 'I have been swimming and taking swimming lessons since I was 6, and I aimed to get my lifeguard certifications. I received my Bronze Medallion, but after that, COVID hit and prevented me from completing it. However, to this day, swimming is still a hobby I enjoy.',
+        description: 'I have been swimming and taking swimming lessons since I was 6, and I aimed to get my lifeguard certifications. I have been swimming for 10 years. I received my Bronze Medallion, but after that, COVID hit and prevented me from completing it. However, to this day, swimming is still a hobby I enjoy.',
         date: '2009-2019',
-        image: Swimming
+        image: [Swimming]
+    },
+    {
+        hobbyName: 'Skating',
+        description: 'I took skating lessons for 10 years. However, when COVID-19 came, I did not get to skate as often anymore.',
+        date: '2009-2019',
+        image: [Skating]
     },
     {
         hobbyName: 'MoveU',
         description: 'MoveU is a UofT Athletics club that promotes healthy living and physical activity. As a first year representative for this club, I planned and participated in various events and activities such as dodgeball, weekly workouts, SheMoves, and many more! It was a great way to stay active and meet new people, while ensuring that students are taking care of their physical health.',
         date: '2021-2022',
-        image: moveU
+        image: [moveU]
     },
     {
         hobbyName: 'Horseback Riding',
         description: 'I took horseback riding lessons.',
         date: '2019-2020',
-        image: Horseback
+        image: [Horseback]
     },
     {
         hobbyName: 'Skateboarding',
         description: 'I enjoy skateboarding.',
         date: '2021-Present',
-        image: SkateBoarding
+        image: [SkateBoarding]
     }
 ]
 
