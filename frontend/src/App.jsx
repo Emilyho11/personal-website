b4c29837381e9674b1cc216b856976b2dd0c6f39
Emily Ho <emilyy.ho@mail.utoronto.ca>
2025-12-03 15:02:50 -0500

    Fixing deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,47 +3,21 @@ import Navbar from './components/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import Projects from './pages/Projects.jsx'
 import Hobbies from './pages/Hobbies.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer.jsx'
 
 function App() {
 
-  // useEffect(() => {
-  //   // Function to disable right-click
-  //   const handleContextMenu = (event) => {
-  //     event.preventDefault();
-  //   };
-    
-  //   // Function to disable specific keyboard shortcuts
-  //   const handleKeyDown = (event) => {
-  //     if (event.keyCode === 123 || // F12
-  //         (event.ctrlKey && event.shiftKey && (event.keyCode === 73 || event.keyCode === 74)) || // Ctrl+Shift+I or Ctrl+Shift+J
-  //         (event.ctrlKey && event.keyCode === 85)) { // Ctrl+U
-  //       event.preventDefault();
-  //     }
-  //   };
-    
-  //   // Add event listeners
-  //   document.addEventListener('contextmenu', handleContextMenu);
-  //   document.addEventListener('keydown', handleKeyDown);
-
-  //   // Clean up event listeners on unmount
-  //   return () => {
-  //     document.removeEventListener('contextmenu', handleContextMenu);
-  //     document.removeEventListener('keydown', handleKeyDown);
-  //   };
-  // }, []);
-
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/hobbies" element={<Hobbies />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <Footer />
     </>
   )
